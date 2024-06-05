7188f187feb8bf818413ba3913bfe50e729fdfbb
Beth002 <elbethelkami9@gmail.com>
2024-06-05 13:44:31 +0200

    LaravelCommit

---
+++ database/migrations/2024_06_04_121722_create_ventes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ventes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('date');
+            $table->double('prix');
+            // REFERENCES MOBILIERS
+            $table->UnsignedBigInteger('mob_id');
+            $table->foreign('mob_id')->references('id')->on('mobiliers');
+            // REFERENCES USERS
+            $table->UnsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            // REFERENCES CLIENTS
+            $table->UnsignedBigInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('clients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ventes');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authcontroller;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [authcontroller::class, 'index']);
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
